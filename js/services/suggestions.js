ca93a5832be9dbccaaa29698f78801b563bc745f
messie28 <tommessie@hotmail.com>
2017-06-22 15:20:20 +0200

    Update suggestions.js

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -4,22 +4,26 @@ app.factory('suggestions', [function(){
 		{
 			title: 'Free pizza at club meetings',
 			upvotes: 15,
-			comments: []
+			comments: [],
+			id: 0
 		},
 		{
 			title: 'End all club emails with Laffy Taffy jokes',
 			upvotes: 9,
-			comments: []
+			comments: [],
+			id: 1
 		},
 		{
 			title: 'Retrofit water fountain with Gatorade',
 			upvotes: 7,
-			comments: []
+			comments: [],
+			id: 2
 		},
 		{
 			title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
 			upvotes: 3,
-			comments: []
+			comments: [],
+			id: 3
 		},
 	]
 	};
